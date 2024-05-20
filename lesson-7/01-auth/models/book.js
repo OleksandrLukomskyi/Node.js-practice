57f36af975eedd2d50beb28690a9a67e53b69984
OleksandrLukomskyi <121460896+OleksandrLukomskyi@users.noreply.github.com>
2024-05-20 08:51:29 +0200

    add

---
+++ lesson-7/01-auth/models/book.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genre: {
+      type: String,
+      enum: [
+        'Action',
+        'Biography',
+        'History',
+        'Horror',
+        'Kids',
+        'Learning',
+        'Sci-Fi',
+      ],
+    },
+    year: { type: Number, defalt: 2024 },
+    pages: { type: Number, required: true },
+  },
+  { versionKey: false, timestamps: true }
+);
+export default mongoose.model('Book', bookSchema);
